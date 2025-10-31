b713e83980fc383d5f6ef5efed792d25713eae67
Lessi <vduomg@gmail.com>
2025-10-31 12:01:11 +0900

    update: rearrange code

--- fixture_pom_test/homeWithFixture.spec.ts
+++ fixture_pom_test/homeWithFixture.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '../helper/fixtures';
 import { expect } from '@playwright/test';
 
-test('Verify user login successfully', async ({ homePage }) => {
+test('Verify user logged in', async ({ homePage }) => {
   await homePage.goTo();
   expect(homePage.isLoggedIn()).toBeTruthy();
 });
